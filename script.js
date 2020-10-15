9a60d1688687abfd1c05276d5fb3010eae083c90
benjaminwcouch <72532063+benjaminwcouch@users.noreply.github.com>
2020-10-15 20:47:08 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function myFunction() {
+   window.open("file:///C:/Users/gado_/Desktop/Clever Programmer Modules/portfolio test/pwj-netflix-clone-master new/index.html")
+  // window.open("https://www.w3schools.com/")
+}
